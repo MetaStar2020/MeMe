4e6bb805ed1631693fe3471054a99bf18b6ada2b
Chantal-Deguire <outercreations@yahoo.com>
2020-04-16 20:47:02 -0400

    Crash when displaying CollectionView

--- MeMe/Controllers/MemeCollectionViewController.swift
+++ MeMe/Controllers/MemeCollectionViewController.swift
@@ -12,6 +12,8 @@ class MemeCollectionViewController: UICollectionViewController {
     
     // MARK: - Properties: Variables and Constants
     
+    @IBOutlet weak var memeCollectionView: UICollectionView!
+    
     let memeCollectionCellID = "MemeCollectionViewCell"
     
     var memes: [Meme]! {
@@ -21,11 +23,15 @@ class MemeCollectionViewController: UICollectionViewController {
     }
     
     // MARK: - View Life Cycle
+    
+     override func viewDidLoad() {
+           super.viewDidLoad()
+       }
      
      override func viewWillAppear(_ animated: Bool) {
          super.viewWillAppear(animated)
-         
-         self.collectionView.reloadData()
+       
+         memeCollectionView!.reloadData()
      }
 
          // MARK: - Required functions for UITableViewDataSource
@@ -38,7 +44,7 @@ class MemeCollectionViewController: UICollectionViewController {
      // Provide a cell object for each row.
      override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         // Fetch a cell of the appropriate type.
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: memeCollectionCellID , for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: memeCollectionCellID , for: indexPath) as! MemeCollectionViewCell
          print(indexPath)
         
         // Configure the cell’s contents.
@@ -46,6 +52,16 @@ class MemeCollectionViewController: UICollectionViewController {
             
         return cell
      }
+    
+    /* as example for DetailView
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let meme = memes[(indexPath as NSIndexPath).row]
+        
+        let memeViewController = self.storyboard!.instantiateViewController(withIdentifier: "MemeViewController") as! MemeViewController
+        memeViewController.meme = meme
+        
+        self.navigationController!.pushViewController(memeViewController, animated: true)
+    }*/
     
     
 }
